f0ead92ce35d5c09fb3d47a33495feff43a4d5d0
Maxwell <nonso2562@562@gmail.com>
2022-10-17 08:25:43 -0700

    Commits

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ * Return: 1 if the number is positive, 0 if otherwise
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of the art,\n");
+	return (0);
+}
