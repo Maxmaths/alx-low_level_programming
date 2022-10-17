9e84d153aca17c5448a8a4a56a51e659af454951
Maxwell <nonso2562@562@gmail.com>
2022-10-17 08:40:46 -0700

    extra commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
-
-
-
 /**
  *
- * *main - ENTRY POINT
- *
- * *Return: Always 0 (Success)
- *
- * */
-
+ *main - ENTRY POINT
+ *Return: Always 0 (Success)
+ */
 
 
 int main(void)
-
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
 
