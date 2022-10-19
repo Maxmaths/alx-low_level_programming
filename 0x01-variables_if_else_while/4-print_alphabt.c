a343bd70a8cbb905be3f926325ebb1710b0f096b
Maxmaths <Nonso2562@gmail.com>
2022-10-19 15:07:32 +0100

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+/**
+  *main - prints alphabet from a to z
+  *Return: returns 0 on success
+  */
+
 int main(void)
-
 {
-	char ch;
+	 char ch;
 
-	for (ch == 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		if (ch == 'q' || ch == 'e')
 		{
