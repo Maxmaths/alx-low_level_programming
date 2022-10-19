605f37c4e6286daf2c4ee653f2a2f15052415ff3
Maxwell <nonso2562@562@gmail.com>
2022-10-19 05:18:33 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 			{
 				putchar((num1 / 10) + 48);
 				putchar((num1 % 10) + 48);
-				putchar(',');
+				putchar(' ');
 				putchar((num2 / 10) + 48);
 				putchar((num2 % 10) + 48);
 
