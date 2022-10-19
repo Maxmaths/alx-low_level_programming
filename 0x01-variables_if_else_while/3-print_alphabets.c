bb50fee2eebb449d376f47c592e1a24238e6e394
Maxwell <nonso2562@562@gmail.com>
2022-10-19 07:01:01 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <ctype.h>
 
+/**
+ *main - prints alphabet from a to z
+ *Return: returns 0 on success
+ */
+
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
