d18a0c641a55dfe4546393468382a8002018a320
Maxwell <nonso2562@562@gmail.com>
2022-10-19 05:00:38 -0700

    extra tasks

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet from a to z
+ * Return: returns 0 on success
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		if (ch <= 9)
+		{
+			putchar(ch = '0');
+		}
+		if (ch < 9)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
