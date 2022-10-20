3ac6cb158db6f731d26b67565744270360fafb39
Maxwell <nonso2562@562@gmail.com>
2022-10-20 05:32:31 -0700

    0x02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ *Return: a-z 
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
+
+/**
+ *print_alphabet_x10 - prints the alphabets 10times with a new line
+ *Return: a-z*10
+ */
+
+void print_alphabet_x10(void)
+{
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+}
