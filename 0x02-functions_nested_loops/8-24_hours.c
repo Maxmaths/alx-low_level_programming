2d291f5a5c07b43e6947968234548324e43ef4c0
Maxwell <nonso2562@562@gmail.com>
2022-10-21 06:44:18 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - time
+ * Return: string
+ */
+
+void jack_bauer(void)
+{
+	int num1,num2;
+
+	for (num1 = 0; num1 < 24; num1++)
+	{
+		for (num2 = 0; num2 < 60; num2++)
+		{
+			_putchar((num1 / 10) + '0');
+			_putchar((num1 % 10) + '0');
+			_putchar(':');
+			_putchar((num2 / 10) + '0');
+			_putchar((num2 % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
