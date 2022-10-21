f44e29ed9a800cff651749fea9242070351d52b6
Maxwell <nonso2562@562@gmail.com>
2022-10-21 06:35:52 -0700

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,8 +4,8 @@ int _abs(int a);
 
 /**
  * _abs - prints the alphabet, followed by a new line.
- * @a: int 
- * Return: the absolute val of the int 
+ * @a: in t
+ * Return: the absolute val of the int
  */
 int _abs(int a)
 {
