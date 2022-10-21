c630a3e2a9735aef62b00a32581071793536f42d
Maxmaths <Nonso2562@gmail.com>
2022-10-21 14:03:08 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,15 +3,15 @@
 int _isalpha(int c);
 
 /**
- *_isalpha - checks if a letter is lowercase
- *@c: is an int
- *Return: returns 0 if true and 1 if false
- */
+  *_isalpha - checks if a letter is lowercase
+  *@c: is an int
+  *Return: returns 0 if true and 1 if false
+  */
 
 int _isalpha(int c)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (c == letter)
