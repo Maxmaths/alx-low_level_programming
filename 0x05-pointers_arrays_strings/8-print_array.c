b77a1a693d1f92b09736a468b40b05fd07120f01
Maxwell <nonso2562@562@gmail.com>
2022-10-25 09:55:08 -0700

    new task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - a function that prints a elements of an array of integers.
+ * 'followed by a new line.
+ * @a: an int
+ * @n: an int
+ * Return: returns 0 on success
+ */
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+		if (x != n - 1)
+		printf("%d, ", a[x]);
+		else
+			printf("%d", a[x]);
+	putchar('\n');
+}
+
