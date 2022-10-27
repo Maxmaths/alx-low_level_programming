0ef5603c6a657728cebf1f70920c212bd2f39df2
Maxwell <nonso2562@562@gmail.com>
2022-10-26 18:21:14 -0700

    tasks

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strins
+ * @dest: a char
+ * @src: a char
+ * Return: returns char on success
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	 char *s = dest;
+
+	 while (*dest != '\0')
+	 {
+		 dest++;
+	 }
+	 while (*src != '\0')
+	 {
+		 *dest = *src;
+		 dest++;
+		 src++;
+	 }
+	 *dest = '\0';
+	 return (s);
+
+}
+
+
